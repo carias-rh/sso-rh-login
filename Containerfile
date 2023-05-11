5fc7f093978806e32af2b4516590bff6f4e0ae76
Carlos M .Arias <carias@redhat.com>
2023-05-11 15:54:54 +0200

    initial commit

---
+++ Containerfile
@@ -0,0 +1,22 @@
+# Use an official Python runtime as the base image
+FROM python:3.9
+
+# Set the working directory in the container
+WORKDIR /app
+
+ENV SECRET
+
+# Copy the requirements file and install dependencies
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+RUN apt-get install oathtool -y
+
+# Copy the application files to the container
+COPY app.py .
+COPY templates templates/
+
+# Expose the container port
+EXPOSE 5000
+
+# Set the entrypoint command to run the Flask application
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+from flask import Flask, render_template
+import subprocess
+
+app = Flask(__name__)
+
+secret = os.getenv('SECRET', default=None)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_otp')
+def get_otp():
+    # Increment the counter in the "counter" file
+    with open('counter', 'r+') as f:
+        counter = int(f.read()) + 1
+        f.seek(0)
+        f.write(str(counter))
+
+    # Execute the command and retrieve the OTP
+    counter = str(os.popen("cat ./counter").read())
+    process = subprocess.Popen(['oathtool', '--hotp', secret, '-c', counter ], stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    return output
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run()
